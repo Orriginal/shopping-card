3202e87edbac16772d05eec402d79a48f0cd6497
Jan Kuipers <jan.kuipers@exact.com>
2024-06-02 15:40:48 +0200

    Changed some configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,61 +23,77 @@ export default [...fixupConfigRules(compat.extends(
     "prettier",
     "plugin:prettier/recommended",
     "plugin:import/recommended",
-)), {
-    plugins: {
-        react: fixupPluginRules(react),
-        "react-hooks": fixupPluginRules(reactHooks),
-    },
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.jest,
+)),
+    {
+        plugins: {
+            react: fixupPluginRules(react),
+            "react-hooks": fixupPluginRules(reactHooks),
         },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.jest,
+            },
 
-        ecmaVersion  : "latest",
-        sourceType   : "module",
+            ecmaVersion  : "latest",
+            sourceType   : "module",
 
-        parser       : typescriptParser,
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
+            parser       : typescriptParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
             },
         },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+            "import/ignore": ["react-native"],
+            "import/resolver": {
+                node: {
+                    extensions: [".js", ".jsx", ".ts", ".tsx"],
+                },
+            }
+        },
+
+        rules: {
+            "no-use-before-define": "off",
+            "@typescript-eslint/no-use-before-define": ["error"],
+
+            "react/jsx-filename-extension": ["warn", {
+                extensions: [".tsx"],
+            }],
+
+            "import/extensions": ["error", "ignorePackages", {
+                ts: "never",
+                tsx: "never",
+            }],
+
+            "no-shadow": "off",
+            "@typescript-eslint/no-shadow": ["error"],
+
+            "import/namespace": "off",
+
+            "@typescript-eslint/explicit-function-return-type": ["error", {
+                allowExpressions: true,
+            }],
+
+            "max-len": ["warn", {
+                code: 100,
+                ignoreComments: true,
+                ignoreUrls: true,
+            }],
+
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+            "import/prefer-default-export": "off",
+            "react/prop-types": "off",
+
+            "prettier/prettier": ["error", {
+                endOfLine: "auto",
+            }],
+        },
     },
-
-    rules: {
-        "no-use-before-define": "off",
-        "@typescript-eslint/no-use-before-define": ["error"],
-
-        "react/jsx-filename-extension": ["warn", {
-            extensions: [".tsx"],
-        }],
-
-        "import/extensions": ["error", "ignorePackages", {
-            ts: "never",
-            tsx: "never",
-        }],
-
-        "no-shadow": "off",
-        "@typescript-eslint/no-shadow": ["error"],
-
-        "@typescript-eslint/explicit-function-return-type": ["error", {
-            allowExpressions: true,
-        }],
-
-        "max-len": ["warn", {
-            code: 100,
-            ignoreComments: true,
-            ignoreUrls: true,
-        }],
-
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "import/prefer-default-export": "off",
-        "react/prop-types": "off",
-
-        "prettier/prettier": ["error", {
-            endOfLine: "auto",
-        }],
-    },
-}];
+];
--- server/dataProvider/giphyDataProvider.ts
+++ server/dataProvider/giphyDataProvider.ts
@@ -1,27 +1,22 @@
 import { TShoppingItem } from '../types/DatabaseTypes';
 import { NumberHelper } from '../../src/helpers/numberHelper';
 
+/**
+ * Get the items from the Giphy API and convert them to shopping items.
+ */
 export class GiphyDataProvider {
     public static async getGifs(): Promise<TShoppingItem[]> {
         const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_KEY}&q=programmer`;
         const response = await fetch(url);
         const data = (await response.json()).data;
         return data.map(
-            ({
-                id,
-                title,
-                embed_url,
-            }: {
-                id: string;
-                title: string;
-                embed_url: string;
-            }): TShoppingItem => ({
+            ({ id, title, embed_url }: { id: string; title: string; embed_url: string }): TShoppingItem => ({
                 id: id,
                 title: title,
                 price: NumberHelper.randomNumber(3, 18),
                 pictureUri: embed_url,
                 stockAmount: NumberHelper.randomNumber(5, 50),
-            }),
+            })
         );
     }
 }
